0495e9d5989f6e89fd7e39e961c36c0aeafa14b0
Funasitien DreamCloud <hello@funa.dev>
2025-04-06 11:30:37 +0000

    🚧 Prettify & API

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [icon(), mdx()]
+  integrations: [icon(), mdx()],
 });
