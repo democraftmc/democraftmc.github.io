80ea651f2bb691934cc96590f3305af1bb892dd2
Funasitien DreamCloud <hello@funa.dev>
2026-01-17 15:42:15 +0100

    ✨ Soon :eyes:

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
-// Styling
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
-
-// Markdown
-import remarkObsidian from 'remark-obsidian';
-import mdx from '@astrojs/mdx';
-
-// Site Managment
-import sitemap from '@astrojs/sitemap'
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wiki.democraft.fr',
-  build: {
-    format: "file",
-  },
-  integrations: [icon(), mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
-  markdown: {
-      syntaxHighlight: {
-        type: 'shiki',
-        excludeLangs: ['mermaid', 'math'],
-      },
-    remarkPlugins: [remarkObsidian],
-  },
-  experimental: {
-    headingIdCompat: true,
-  }
+  integrations: [icon(), mdx()]
 });
